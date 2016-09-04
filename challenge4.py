67d1ac1c851056bc1fe1df525adf46a1557f35c4
Dario Molina <dariomolina@student.hartnell.edu>
2016-09-03 21:17:07 -0700

    Program that makes a request to Code2040's API retrieving a dictionary.  Dictionary has keys 'prefix' and 'array'. Prefix is a string and array is an array of strings.  Successfully returned an array containing only the strings that do not start with the prefix in the array 'array'.

--- challenge4.py
+++ challenge4.py
@@ -3,10 +3,8 @@ import requests
 
 """
 Name: Dario Molina
-9/3/16
-Description: Write a program that connects to Code2040 API, retrive a dictionary for The first value, prefix, is a string. The second value, array, is an array of strings. Your job is to return an array containing only the strings that do not start with that prefix. 
-
-Once new array is built, POST a dicitonary back to API and check for varification.
+Name: 9/3/16
+Description: Write a program that connects to Code2040 API. Then retrive a dictionary with keys prefix and array.  Prefix, is a string and array is an array of strings. Your job is to return an array containing only the strings that do not start with that prefix. Once new array is built, POST a dicitonary back to API and check for varification.
 """
 #URl's where we obtain and send information to Code2040's API
 retrieve_info_url = "http://challenge.code2040.org/api/prefix"
