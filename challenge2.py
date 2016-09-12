d9fe48fc2b259d363594ad495a6b6907253c8819
Dario Molina <dariomolina@student.hartnell.edu>
2016-09-11 20:32:13 -0700

    Added some extra comments and restructured some of the previous comments.

--- challenge2.py
+++ challenge2.py
@@ -1,24 +1,22 @@
+#Module that will allow us to create our json dictionary.
 import json
+#Module that will allow us to connect to the API.
 import requests
 
 """
 Name: Dario Molina
 8/30/16
-Description: Connect to Code2040 API, receive a string, reverse the string then send the reversed word back through a request to the desired destination.
+Description: Connect to Code2040 API, receive a string, reverse the string then send the reversed string back through a request for verification.
 """
 
-#URL where we obtain and send information from the API
+#URL where we obtain and send information to the API
 retrieve_info_url = "http://challenge.code2040.org/api/reverse"
 send_info_url = "http://challenge.code2040.org/api/reverse/validate"
 
-#token to connect to the API
+#Our API token
 accessKey = "37bc2120300edb47c357ab11625cbfa7"
 
-#Github repository URLclear
-github_url = "https://github.com/dariomolina93/Code2040Assesment"
-
-
-#Creating a dictionary where it contains our access token, github account, that will allow us to connect to the API
+#Creating a dictionary where it contains our access token, that will allow us to connect to the API
 retrieve_info_dictionary ={"token":accessKey}
 
 #Obtaining the information from the API and storing it in variable r
@@ -30,14 +28,14 @@ print r.text
 #Using the "extended slice operator", it reverses the string and stores it in our new variable "reversedWord" 
 reversedWord = r.text[::-1]
 
-#dictionary where we will store our reversedWord 
+#dictionary where we will store our reversedWord, so we can send back to the API for verification.
 send_info_dictionary = {"token": accessKey, "string": reversedWord}
 
 
-#sending the request to the desired url with the dictitionary in json format.
+#sending request to API for verification that we passed this challenge.
 t = requests.post(send_info_url, json = send_info_dictionary)
 
-#printing status of our t request
+#printing contents of our t request
 print t.text
 print t.status_code
 
